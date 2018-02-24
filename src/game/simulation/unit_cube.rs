776414012a8d90a978b8917d50d340e58e68600c
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2018-02-24 21:49:59 +0200

    Implement unit-cube

---
+++ src/game/simulation/unit_cube.rs
@@ -0,0 +1,56 @@
+use util::*;
+
+lazy_static! {
+    pub static ref UNIT_CUBE_VBO: Vec<VertexPN> = {
+        UNIT_CUBE_VERT.chunks(3)
+            .map(|chunk| VertexPN {position: [chunk[0], chunk[1], chunk[2]], normal: [1f32, 1f32, 1f32]})
+            .collect::<Vec<VertexPN>>()
+    };
+}
+
+pub const UNIT_CUBE_VERT: [f32; 6 /* faces */ * 4 /* vertices */ * 3 /* dimensions */ ] = [
+    // Front face
+    -1.0, -1.0,  1.0,
+    1.0, -1.0,  1.0,
+    1.0,  1.0,  1.0,
+    -1.0,  1.0,  1.0,
+
+    // Back face
+    -1.0, -1.0, -1.0,
+    -1.0,  1.0, -1.0,
+    1.0,  1.0, -1.0,
+    1.0, -1.0, -1.0,
+
+    // Top face
+    -1.0,  1.0, -1.0,
+    -1.0,  1.0,  1.0,
+    1.0,  1.0,  1.0,
+    1.0,  1.0, -1.0,
+
+    // Bottom face
+    -1.0, -1.0, -1.0,
+    1.0, -1.0, -1.0,
+    1.0, -1.0,  1.0,
+    -1.0, -1.0,  1.0,
+
+    // Right face
+    1.0, -1.0, -1.0,
+    1.0,  1.0, -1.0,
+    1.0,  1.0,  1.0,
+    1.0, -1.0,  1.0,
+
+    // Left face
+    -1.0, -1.0, -1.0,
+    -1.0, -1.0,  1.0,
+    -1.0,  1.0,  1.0,
+    -1.0,  1.0, -1.0,
+];
+
+pub const UNIT_CUBE_IBO: [u32; 6 * 2 * 3] = [
+    0,  1,  2,      0,  2,  3,    // front
+    4,  5,  6,      4,  6,  7,    // back
+    8,  9,  10,     8,  10, 11,   // top
+    12, 13, 14,     12, 14, 15,   // bottom
+    16, 17, 18,     16, 18, 19,   // right
+    20, 21, 22,     20, 22, 23,   // left
+];
